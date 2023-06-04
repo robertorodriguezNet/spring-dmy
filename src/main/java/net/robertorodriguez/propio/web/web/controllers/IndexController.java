afad1f8955a65ca35b8fd2cf38b1ab8c00ede653
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-06-04 16:14:24 +0200

    first commit

---
+++ src/main/java/net/robertorodriguez/propio/web/web/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package net.robertorodriguez.propio.web.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"/index","/","/home"})
+    public String index(){
+        return "index";
+    }
+    
+}
